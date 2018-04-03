70896848516a890f4d0b4fed40f2d51167e662d5
Jean-Marie Burel <j.burel@dundee.ac.uk>
2018-04-03 19:25:36 +0100

    Improve script and include cut_link

--- maintenance/cleanup.py
+++ maintenance/cleanup.py
@@ -23,8 +23,9 @@
 """
 This script first finds and lists, then cleans up objects
 (ROIs, Ratings and Tags) attached to all images contained in a Dataset with a
-specified name ("Condensation") belonging to users user-1 through user-40
+specified name belonging to users user-1 through user-40
 and to "trainer-1".
+The script assumes that all users have the same password.
 The listing is done by each user on their own data.
 The information is collected and the final cleanup is done by an
 administrator ("trainer-1"), who makes sure that also objects linked to images
@@ -34,31 +35,54 @@ The cleanup of Tags just deletes the link between the Tag and the image,
 thus removing the Tag, but not deleting it.
 """
 
-
+import argparse
 import omero
 from omero.gateway import BlitzGateway
 from omero.rtypes import wrap
 
+obj_ids_taglinks = []
+obj_ids_rating = []
+obj_ids_rois = []
 
-def list_objs(username, password, host):
 
-    global mitosis_tags_nr
-    conn = BlitzGateway(username, password, host=host, port=4064)
-    conn.connect()
+def cut_link(conn, username, image):
+    params = omero.sys.ParametersI()
+    params.addString('username', username)
+    query = "from Image where name='%s' \
+            AND details.owner.omeName=:username" % image
+    query_service = conn.getQueryService()
+    images = query_service.findAllByQuery(query, params,
+                                          conn.SERVICE_OPTS)
+    if len(images) == 0:
+        print "No image with name %s found" % image
+    params = omero.sys.ParametersI()
+    params.addLong('imageId', images[0].getId().getValue())
+    query = "from DatasetImageLink where child.id=:imageId"
+    links = conn.getQueryService().findAllByQuery(query, params,
+                                                  conn.SERVICE_OPTS)
+    if len(links) > 0:
+        ids = []
+        for link in links:
+            ids.append(link.getId().getValue())
+        conn.deleteObjects("DatasetImageLink", ids, deleteAnns=True,
+                           deleteChildren=False, wait=True)
+
+
+def list_objs(conn, username, target):
 
     params = omero.sys.ParametersI()
     params.addString('username', username)
-    query = "from Dataset where name='Condensation' \
-             AND details.owner.omeName=:username"
+    query = "from Dataset where name='%s' \
+             AND details.owner.omeName=:username" % target
     query_service = conn.getQueryService()
-    dataset = query_service.findAllByQuery(query, params, conn.SERVICE_OPTS)
-    dataset_id = dataset[0].getId().getValue()
+    datasets = query_service.findAllByQuery(query, params, conn.SERVICE_OPTS)
+    if len(datasets) == 0:
+        print "No dataset with name %s found" % target
 
+    dataset_id = datasets[0].getId().getValue()
     dataset = conn.getObject("Dataset", dataset_id)
-    k = 0
     roi_service = conn.getRoiService()
     for image in dataset.listChildren():
-        k += 1
         result = roi_service.findByImage(image.getId(), None)
 
         for roi in result.rois:
@@ -76,40 +100,24 @@ def list_objs(username, password, host):
                 value = 'Will delete rating %s on image \
                         %s of %s' % (ann.getId(), image.getId(), username)
                 print value
-
-            if ann.OMERO_TYPE == omero.model.TagAnnotationI:
-                if ann.getId() == 4236:
-                    mitosis_tags_nr += 1
-                else:
-                    params2 = omero.sys.ParametersI()
-                    params2.add('imageId', wrap(image.getId()))
-                    params2.addId(ann.getId())
-                    query = "select l.id from ImageAnnotationLink as \
-                            l where l.parent.id=:imageId AND l.child.id=:id"
-                    linkIds = query_service.projection(query, params2,
-                                                       conn.SERVICE_OPTS)
-                    for linkId in linkIds:
-                        obj_ids_taglinks.append(linkId[0].getValue())
-                        value = 'Will delete link %s on image \
-                                %s of tag %s of ' % (linkId[0].getValue(),
-                                                     image.getId(),
-                                                     ann.getId(), username)
-                        print value
-
-    if not mitosis_tags_nr == 6:
-        print 'WARNING mitosis_tags_nr is %s %s' % (mitosis_tags_nr, username)
-    mitosis_tags_nr = 0
-    if not k == 32:
-        value = 'WARNING dataset %s of %s \
-                has %s images' % (dataset_id, username, k)
-        print value
-
-    conn.close()
+            elif ann.OMERO_TYPE == omero.model.TagAnnotationI:
+                params = omero.sys.ParametersI()
+                params.add('imageId', wrap(image.getId()))
+                params.addId(ann.getId())
+                query = "select l.id from ImageAnnotationLink as \
+                        l where l.parent.id=:imageId AND l.child.id=:id"
+                linkIds = query_service.projection(query, params,
+                                                   conn.SERVICE_OPTS)
+                for linkId in linkIds:
+                    obj_ids_taglinks.append(linkId[0].getValue())
+                    value = 'Will delete link %s on image \
+                            %s of tag %s of %s' % (linkId[0].getValue(),
+                                                 image.getId(),
+                                                 ann.getId(), username)
+                    print value
 
 
-def delete_objs(username, password, host):
-    conn = BlitzGateway(username, password, host=host, port=4064)
-    conn.connect()
+def delete_objs(conn):
     print 'deleting', len(obj_ids_taglinks), ' tag links'
     if len(obj_ids_taglinks) > 0:
         conn.deleteObjects("ImageAnnotationLink", obj_ids_taglinks,
@@ -122,20 +130,47 @@ def delete_objs(username, password, host):
     if len(obj_ids_rating) > 0:
         conn.deleteObjects("LongAnnotation", obj_ids_rating, deleteAnns=True,
                            deleteChildren=False, wait=True)
-    conn.close()
 
 
-password = "password"
-host = "outreach.openmicroscopy.org"
-obj_ids_taglinks = []
-obj_ids_rating = []
-obj_ids_rois = []
-mitosis_tags_nr = 0
+def run(password, target, image, host, port):
+   
+    for i in range(1, 41):
+        username = "user-%s" % i
+        try:
+            conn = BlitzGateway(username, password, host=host, port=port)
+            conn.connect()
+            list_objs(conn, username, target)
+            cut_link(conn, username, image)
+        except Exception as exc:
+            print "Error while cleaning the objects: %s" % str(exc)
+        finally:
+            conn.close()
 
-for i in range(1, 41):
-    username = "user-%s" % i
-    list_objs(username, password, host)
+    trainer_name = "trainer-1"
+    try:
+        conn = BlitzGateway(trainer_name, password, host=host, port=port)
+        conn.connect()
+        list_objs(conn, trainer_name, target)
+        cut_link(conn, trainer_name, image)
+        delete_objs(conn)
+    except Exception as exc:
+        print "Error while cleaning the objects: %s" % str(exc)
+    finally:
+        conn.close()
 
-trainer_name = "trainer-1"
-list_objs(trainer_name, password, host)
-delete_objs(trainer_name, password, host)
+
+def main(args):
+    parser = argparse.ArgumentParser()
+    parser.add_argument('password')
+    parser.add_argument('dataset')
+    parser.add_argument('image')
+    parser.add_argument('--server', default="outreach.openmicroscopy.org",
+                        help="OMERO server hostname")
+    parser.add_argument('--port', default=4064, help="OMERO server port")
+    args = parser.parse_args(args)
+    run(args.password, args.dataset, args.image, args.server, args.port)
+
+
+if __name__ == '__main__':
+    import sys
+    main(sys.argv[1:])
