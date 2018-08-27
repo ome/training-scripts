219b6ef7fb80f595efe8ab9229ffa15d92b1bf2d
Jean-Marie Burel <j.burel@dundee.ac.uk>
2018-08-27 14:42:27 +0100

    Example script showing how to use the CLI commands

---
+++ practical/bash/import_as_create_annotation.sh
@@ -0,0 +1,85 @@
+#!/bin/bash
+# -----------------------------------------------------------------------------
+#   Copyright (C) 2018 University of Dundee. All rights reserved.
+#
+#   This program is free software; you can redistribute it and/or modify
+#   it under the terms of the GNU General Public License as published by
+#   the Free Software Foundation; either version 2 of the License, or
+#   (at your option) any later version.
+#   This program is distributed in the hope that it will be useful,
+#   but WITHOUT ANY WARRANTY; without even the implied warranty of
+#   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#   GNU General Public License for more details.
+#
+#   You should have received a copy of the GNU General Public License along
+#   with this program; if not, write to the Free Software Foundation, Inc.,
+#   51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+#
+# ------------------------------------------------------------------------------
+#
+# In this script we:
+#   * log in as another user
+#   * create a new dataset
+#   * import an image
+#   * upload a attachment
+#
+#
+dir=$(pwd -P)
+OMEROPATH=${OMEROPATH:-/opt/omero/server/OMERO.server/bin/omero}
+PASSWORD=${PASSWORD:-ome}
+HOST=${HOST:-outreach.openmicroscopy.org}
+SUDOER=${SUDOER:-trainer-1}
+OMEUSER=${OMEUSER:-user-1}
+IMAGEPATH=${IMAGEPATH:-/}
+ATTACHMENTPATH=${ATTACHMENTPATH:-/}
+
+deleteimage=false
+deletefile=false
+
+if [ "$IMAGEPATH"="/" ]; then
+    touch image_to_import.fake
+    IMAGEPATH=$dir/image_to_import.fake
+    deleteimage=true
+fi
+#printf $IMAGEPATH
+
+if [ "$ATTACHMENTPATH"="/" ]; then
+    touch file_to_upload.csv
+    ATTACHMENTPATH=$dir/file_to_upload.csv
+    deletefile=true
+fi
+
+# Log in as
+$OMEROPATH login --sudo ${SUDOER} -u $OMEUSER -s $HOST -w $PASSWORD
+
+# Create a dataset as the specified user
+dataset=`$OMEROPATH obj new Dataset name='Basel-workflow'`
+
+# Import the image in the newly created dataset
+result=`$OMEROPATH import -T $dataset $IMAGEPATH --output ids`
+
+# Retrieve the Image's id. it is Image:123
+imageid=`cut -d':' -f2 <<< $result`
+printf 'imageid %s \n' "$id"
+
+# Upload a CSV and attach it to the Image
+result=`$OMEROPATH upload $ATTACHMENTPATH`
+originalfileid=`cut -d':' -f2 <<< $result`
+printf 'originalfileid %s \n' "$originalfileid"
+
+# Create a file annotatin
+result=`$OMEROPATH obj new FileAnnotation file=OriginalFile:$originalfileid`
+fileid=`cut -d':' -f2 <<< $result`
+printf 'fileid %s \n' "$fileid"
+
+# Link the annotation to the Image
+$OMEROPATH obj new ImageAnnotationLink parent=Image:$imageid child=FileAnnotation:$fileid
+
+# delete the Image and the local file if created
+if [ $deleteimage = true ]; then
+    rm $IMAGEPATH
+fi
+
+if [ $deletefile = true ]; then
+    rm $ATTACHMENTPATH
+fi
