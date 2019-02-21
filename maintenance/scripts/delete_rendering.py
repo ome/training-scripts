e4edef0f3c6e8d42d9ae839ca549f20d753a5535
pwalczysko <p.walczysko@dundee.ac.uk>
2019-02-21 16:06:56 +0000

    Clean up, delete commented out code

--- maintenance/scripts/delete_rendering.py
+++ maintenance/scripts/delete_rendering.py
@@ -20,7 +20,8 @@
 # ------------------------------------------------------------------------------
 
 """
-This script 
+This script finds all rendering on images from a dataset specified
+by dataset id or dataset name and deletes these rendering settings.
 """
 
 import argparse
@@ -33,7 +34,6 @@ def run(name, password, dataset_name, dataset_id, host, port):
     conn = BlitzGateway(name, password, host=host, port=port)
     try:
         conn.connect()
-        roi_service = conn.getRoiService()
         rnd_service = conn.getRenderingSettingsService()
         datasets = []
         if dataset_id >= 0:
@@ -41,24 +41,18 @@ def run(name, password, dataset_name, dataset_id, host, port):
         else:
             datasets = conn.getObjects("Dataset",
                                        attributes={"name": dataset_name})
-        
-        print datasets
 
         for dataset in datasets:
             print dataset.getId()
             for image in dataset.listChildren():
                 pixels = image.getPrimaryPixels()
-                result = roi_service.findByImage(image.getId(), None,
-                                                 conn.SERVICE_OPTS)
-                #result2 = rnd_service.findByImage(image.getId(), None, conn.SERVICE_OPTS)
                 print pixels.getId()
                 params = omero.sys.ParametersI()
                 query = "from RenderingDef where pixels.id = '%s'" % pixels.getId()
                 query_service = conn.getQueryService()
-                result2 = query_service.findAllByQuery(query, params, conn.SERVICE_OPTS)
-                #print result2
-                if result2 is not None:
-                    rnd_ids = [rnd.id.val for rnd in result2]
+                result = query_service.findAllByQuery(query, params, conn.SERVICE_OPTS)
+                if result is not None:
+                    rnd_ids = [rnd.id.val for rnd in result]
                     if len(rnd_ids) > 0:
                         print "Deleting %s rnds..." % len(rnd_ids)
                         print rnd_ids
