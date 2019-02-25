24c7a9b166cefc691351bf1f2c7f22e378713d03
pwalczysko <p.walczysko@dundee.ac.uk>
2019-02-25 13:18:22 +0000

    Rewrite the script for bash as per Mark's comment

---
+++ maintenance/scripts/delete_rendering.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+#
+# This script deletes rnd settings on images in specified Datasets.
+# Datasets can be specified either by name, such as
+# DATASETNAME=svs bash delete_rendering.sh
+# or by IDs (one or more, separated by comma), such as
+# DATASETID=1868,2073 bash delete_rendering.sh
+
+echo Starting
+OMEROPATH=${OMEROPATH:-/opt/omero/server/OMERO.server/bin/omero}
+PASSWORD=${PASSWORD:-ome}
+HOST=${HOST:-outreach.openmicroscopy.org}
+DATASETNAME=${DATASETNAME:-siRNAi-HeLa}
+DATASETIDS=${DATASETID:-none}
+NUMBER=${NUMBER:-50}
+OMEUSER=${OMEUSER:-trainer-1}
+
+$OMEROPATH login -u $OMEUSER -s $HOST -w $PASSWORD
+result=`$OMEROPATH hql --ids-only --limit 1000 --style plain -q --all "SELECT id from Dataset WHERE name = '$DATASETNAME'"`
+if [ "$DATASETIDS" = "none" ]
+then
+    echo "none ID"
+    for i in $result
+    do
+        #remove the ordinal numbers
+        datasetids_raw+=${i#*,}
+        datasetids_raw+=","
+    done
+    dataset_number=${i%,*}
+    #remove the trailing comma
+    datasetids=${datasetids_raw%?}
+else
+    datasetids=$DATASETIDS
+    dataset_number="specified datasets"
+fi
+echo 'Deleting rnd settings on '"$dataset_number"' Datasets:' $datasetids
+$OMEROPATH delete --report Dataset/RenderingDef:$datasetids
+$OMEROPATH logout
+echo Stopping
