46acd053a4c88fe6f9019a58ed923a0ae3200a69
William Moore <w.moore@dundee.ac.uk>
2021-04-26 09:51:57 +0100

    calibrate_images.py target supports Project:ID

--- maintenance/scripts/calibrate_images.py
+++ maintenance/scripts/calibrate_images.py
@@ -33,9 +33,50 @@ from omero.gateway import BlitzGateway
 from omero.model.enums import UnitsLength
 
 
-def run(password, target, host, port):
+def setPixelSize(conn, image, value, unit):
+    print('Image', image.id, value)
+    value_z = None
+    if 'x' in value:
+        value_xy = float(value.split('x')[0])
+        value_z = float(value.split('x')[1])
+    else:
+        value_xy = float(value)
+    xy = omero.model.LengthI(value_xy, getattr(UnitsLength, unit))
+    p = image.getPrimaryPixels()._obj
+    p.setPhysicalSizeX(xy)
+    p.setPhysicalSizeY(xy)
+    if value_z is not None:
+        z = omero.model.LengthI(value_z, getattr(UnitsLength, unit))
+        p.setPhysicalSizeZ(z)
+    conn.getUpdateService().saveObject(p)
 
-    for i in range(1, 51):
+
+def set_by_target_id(password, username, target, host, port, value, unit):
+
+    conn = BlitzGateway(username, password, host=host, port=port)
+    conn.connect()
+    target_type = target.split(':')[0]
+    target_id = int(target.split(':')[1])
+    if target_type not in ['Project', 'Dataset', 'Image']:
+        print("Target must be Project:ID, Dataset:ID or Image:ID")
+        return
+    images = []
+    if target_type == "Project":
+        for dataset in conn.getObject('Project', target_id).listChildren():
+            images.extend((list(dataset.listChildren())))
+    elif target_type == "Dataset":
+        images = list(conn.getObject('Dataset', target_id).listChildren())
+    elif target_type == "Image":
+        images = [conn.getObject("Image", target_id)]
+    print('images', images)
+
+    for image in images:
+        setPixelSize(conn, image, value, unit)
+
+
+def set_for_users_by_dataset_name(password, target, host, port, value, unit):
+
+    for i in range(1, 2):
 
         username = "user-%s" % i
         print(username)
@@ -56,42 +97,61 @@ def run(password, target, host, port):
 
             dataset_obj = dataset[0]
             datasetId = dataset[0].getId().getValue()
-
             print('dataset', datasetId)
-            params2 = omero.sys.ParametersI()
-            params2.addId(dataset_obj.getId())
-            query = "select l.child.id from DatasetImageLink \
-                     l where l.parent.id = :id"
-            images = service.projection(query, params2, conn.SERVICE_OPTS)
-            values = []
-            for k in range(0, len(images)):
-
-                image_id = images[k][0].getValue()
-                image = conn.getObject("Image", image_id)
-
-                u = omero.model.LengthI(0.33, UnitsLength.MICROMETER)
-                p = image.getPrimaryPixels()._obj
-                p.setPhysicalSizeX(u)
-                p.setPhysicalSizeY(u)
-                values.append(p)
-
-            if len(images) > 0:
-                conn.getUpdateService().saveArray(values)
+            for image in conn.getObject("Dataset", datasetId).listChildren():
+                setPixelSize(conn, image, value, unit)
         except Exception as exc:
             print("Error during calibration: %s" % str(exc))
         finally:
             conn.close()
 
+def run(args):
+    password = args.password
+    target = args.target
+    value = args.value
+    unit = args.unit
+    host = args.server
+    port = args.port
+
+    # Handle target is e.g. "Project:1"
+    if ':' in target:
+        try:
+            target_id = int(target.split(':')[1])
+            set_by_target_id(password, args.user, target, host, port, value, unit)
+            return
+        except ValueError:
+            print("Not valid Project or Dataset ID")
+
+    # Assume that target was a Dataset Name
+    set_for_users_by_dataset_name(password, target, host, port, value, unit)
 
 def main(args):
+    """
+    The script sets Pixels Sizes in 2 use-cases. 
+    Each needs a pixel size 'value' eg. 0.85 for X and Y or '0.85x0.2' for XY and Z
+    Units are optional. Default is "MICROMETER".
+
+    1) For many users 'user-1...user-50' etc with a NAMED Dataset
+    $ calibrate_images.py [password] [dataset_name] [value] --server [server]
+
+    2) For a single user, where the target is Project:ID or Dataset:ID
+    $ calibrate_images.py [password] [target] [value] --user [username] --server [server]
+    """
     parser = argparse.ArgumentParser()
     parser.add_argument('password')
-    parser.add_argument('target')
-    parser.add_argument('--server', default="workshop.openmicroscopy.org",
+    parser.add_argument(
+        'target',
+        help="Dataset name (for many users) or target Project/Dataset/Image:ID")
+    parser.add_argument('value', help="Pixel size value")
+    parser.add_argument('--unit', default="MICROMETER",
+        help="Unit from omero.")
+    parser.add_argument('--user', help="Username ONLY if single user")
+    parser.add_argument('--server', default="localhost",
                         help="OMERO server hostname")
     parser.add_argument('--port', default=4064, help="OMERO server port")
     args = parser.parse_args(args)
-    run(args.password, args.target, args.server, args.port)
+
+    run(args)
 
 
 if __name__ == '__main__':
